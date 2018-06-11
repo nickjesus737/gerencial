6e6827a9d5974fa1c78932e328e7692c233a065e
Jesus Acendra <jaam737@hotmail.com>
2018-06-11 17:11:29 -0500

    pequeños cambios a ganttChart

--- scripts/charts/ganttChart.js
+++ scripts/charts/ganttChart.js
@@ -19,13 +19,22 @@ function drawChart() {
     data.addRows([
         ['1', 'Modelado de Negocio', 'inicio', new Date(2000, 0, 1), new Date(2000, 1, 29), null, 100, null],
         ['2', 'Requisitos', 'inicio', new Date(2000, 1, 15), new Date(2000, 3, 31), null, 25, '1'],
-        ['3', 'Analisis y Diseño', 'elaboracion', new Date(2000, 1, 20), new Date(2000, 4, 15), null, 20, '1,2'],
+        ['3', 'Analisis y Diseño', 'elaboracion', new Date(2000, 1, 29), new Date(2000, 4, 15), null, 20, '1,2'],
         ['4', 'Implementación', 'construccion', new Date(2000, 4, 1), new Date(2000, 8, 30), null, 0, '3'],
         ['5', 'Pruebas', 'transicion', new Date(2000, 1, 1), new Date(2000, 9, 31), null, 10, null],
         ['6', 'Despliegue', 'transicion', new Date(2000, 9, 1), new Date(2000, 10, 30), null, 0, '4,5']
     ]);
 
     var options = {
+        gantt: {
+            criticalPathEnabled: false, // Critical path arrows will be the same as other arrows.
+            arrow: {
+              angle: 100,
+              width: 2,
+              color: 'black',
+              radius: 0
+            }
+          }
     };
 
     var chart = new google.visualization.Gantt(document.getElementById('ganttChart'));
